e8cf5ed53e062b25222e7bcb17e0aeb9df8232b5
devui3 <devui3@qoobus.com>
2023-06-20 11:34:57 +0300

    add adaptive for navblock

--- components/UI/InfoCard.tsx/InfoCard.tsx
+++ components/UI/InfoCard.tsx/InfoCard.tsx
@@ -7,6 +7,7 @@ interface ComponentProps {
   desc: string;
   img?: string;
   imgStyle?: string;
+  imgWrapper?: string;
   flex?: string;
 }
 
@@ -15,6 +16,7 @@ const InfoCard: React.FC<ComponentProps> = ({
   desc,
   img,
   imgStyle,
+  imgWrapper,
   flex,
 }) => {
   function clampString(inputString: string): string {
@@ -29,31 +31,33 @@ const InfoCard: React.FC<ComponentProps> = ({
   }
 
   return (
-    <div className="overflow-hidden">
-      <Button
-        outsideGradient={
-          "rounded-[19px] hover:bg-gradient-to-r from-[#080809]  to-[#c6b38a] p-[2px]"
-        }
-        insideGradient={"block rounded-[19px] mobile-button-color p-[1px]"}
-      >
-        <div className={`${flex} relative`}>
-          <div className={imgStyle}>
-            {img ? <Image src={img} alt="" /> : ""}
-          </div>
-          <div className="flex flex-col w-full items-start gap-[37px] p-[40px]">
-            <h3 className="text-[40px] ">{title}</h3>
-            <p className="text-[20px]  text-[#838383] font-man max-w-[700px]">
-              {clampString(desc)}
-            </p>
-            <Button>
-              <button className="mx-[60px] my-[14px] text-[20px] font-man">
-                View more
-              </button>
-            </Button>
-          </div>
+    <Button
+      outsideGradient={
+        "overflow-hidden rounded-[19px] hover:bg-gradient-to-r from-[#080809]  to-[#c6b38a] p-[2px]"
+      }
+      insideGradient={"block rounded-[19px] mobile-button-color p-[1px]"}
+    >
+      <div className={`${flex} relative`}>
+        <div className={imgWrapper}>
+          {img ? <Image src={img} className={imgStyle} alt="" /> : ""}
         </div>
-      </Button>
-    </div>
+        <div className="flex flex-col w-full items-start gap-[17px] md:gap-[37px] p-[20px] md:p-[40px]">
+          <h3 className="text-[32px] md:text-[40px] ">{title}</h3>
+          <p className="md:text-[20px]  text-[#838383] font-man max-w-[700px]">
+            {clampString(desc)}
+          </p>
+          <Button
+            outsideGradient={
+              "mt-[10px] bg-gradient-to-r from-[#b5713f]  to-[#c6b38a] p-[2px]"
+            }
+          >
+            <button className="mx-[112px] my-[10px] md:mx-[60px] md:my-[14px] md:text-[20px] font-man">
+              View more
+            </button>
+          </Button>
+        </div>
+      </div>
+    </Button>
   );
 };
 
