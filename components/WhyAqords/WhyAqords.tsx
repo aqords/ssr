358127b4a624faf9ffb9ead6e16645b3a3d5d26f
devui3 <devui3@qoobus.com>
2023-06-22 11:14:00 +0300

    add full responsive

--- components/WhyAqords/WhyAqords.tsx
+++ components/WhyAqords/WhyAqords.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
 import ItemsList from "./WhyAqordsList";
+import ScrollToggle from "../../utils/hooks/ScrollToggle";
+import useScrollClassChange from "../../utils/hooks/useScrollChange";
 
 const WhyAqords = () => {
+  const isScrolled = ScrollToggle({
+    targetClasses: ["el-1", "el-2", "el-3"],
+    visibleClass: "visible",
+    originalClass: "original",
+  });
+
+  const elementRef = useRef<HTMLDivElement>(null);
+  const elementRef2 = useRef<HTMLDivElement>(null);
+  const elementRef3 = useRef<HTMLDivElement>(null);
+
+  useScrollClassChange({
+    elementRefs: [elementRef, elementRef2, elementRef3],
+    className: "original",
+  });
+
   return (
     <>
       <main>
         <section className="bg-second">
           <div className="container">
             <div className="pt-[100px] sm:pt-[150px] md:pt-[320px]">
-              <h2 className="  font-bold text-2xl md:text-[48px]  mb-[43px] tracking-[-1.5px]">
-                Why AQORDS?
-              </h2>
-              <p className="font-normal  text-base md:text-lg font-man max-w-[790px] leading-6 md:leading-8 tracking-[-0.42px] ">
-                AQORDS revolutionizes the freight transportation industry with
-                blockchain-powered solutions and unprecedented opportunities.
-              </p>
+              <div
+                ref={elementRef}
+                className={
+                  isScrolled["el-1"] ? "el-1 visible" : "el-1 original"
+                }
+              >
+                <h2 className="  font-bold text-2xl md:text-[48px]  mb-[43px] tracking-[-1.5px]">
+                  Why AQORDS?
+                </h2>
+                <p className="font-normal  text-base md:text-lg font-man max-w-[790px] leading-6 md:leading-8 tracking-[-0.42px] ">
+                  AQORDS revolutionizes the freight transportation industry with
+                  blockchain-powered solutions and unprecedented opportunities.
+                </p>
+              </div>
             </div>
             <ItemsList />
           </div>
@@ -23,15 +47,29 @@ const WhyAqords = () => {
         <section className=" bg-no-repeat bg-cover  bg-[url('../public/assets/images/truck.png')] sm:bg-center ">
           <div className="container">
             <div className="  py-[22%] w-full max-w-[330px] md:max-w-[530px]">
-              <h3 className="tracking-[-1.5px] max-w-[280px] md:max-w-[537px] text-2xl leading-10 md:leading-[59px] font-bold  md:text-[48px] mb-[30px]">
-                Are you prepared to embark on this transformative journey?
-              </h3>
-              <p className=" tracking-[-0.14px] font-man  font-normal md:text-[20px] text-base leading-[24px]  md:leading-[32px] ">
-                Delve into our website to unravel the intricate workings of our
-                platform, acquaint yourself with our exceptional team, and
-                witness firsthand how AQORDS propels your freight transportation
-                operations into a new era.
-              </p>
+              <div
+                ref={elementRef2}
+                className={
+                  isScrolled["el-2"] ? "el-2 visible" : "el-2 original"
+                }
+              >
+                <h3 className="tracking-[-1.5px] max-w-[280px] md:max-w-[537px] text-2xl leading-10 md:leading-[59px] font-bold  md:text-[48px] mb-[30px]">
+                  Are you prepared to embark on this transformative journey?
+                </h3>
+              </div>
+              <div
+                ref={elementRef3}
+                className={
+                  isScrolled["el-3"] ? "el-3 visible" : "el-3 original"
+                }
+              >
+                <p className=" tracking-[-0.14px] font-man  font-normal md:text-[20px] text-base leading-[24px]  md:leading-[32px] ">
+                  Delve into our website to unravel the intricate workings of
+                  our platform, acquaint yourself with our exceptional team, and
+                  witness firsthand how AQORDS propels your freight
+                  transportation operations into a new era.
+                </p>
+              </div>
             </div>
           </div>
         </section>
