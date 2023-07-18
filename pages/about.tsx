2f1ceff26f4b52311f209e4cb4f31775c63a2d5f
devui3 <devui3@qoobus.com>
2023-07-18 11:35:19 +0300

    fix

--- pages/about.tsx
+++ pages/about.tsx
@@ -22,8 +22,14 @@ const about = () => {
       <Head>
         <title>{t("about_title")}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={t("about_description")} />
-        <meta name="theme-color" content="default" />
+        <meta name="description" content="home_description" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&family=Rubik:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="theme-color" content="default"></meta>
       </Head>
       <Header />
       <BlockWithBackground
--- pages/manifesto.tsx
+++ pages/manifesto.tsx
@@ -35,9 +35,18 @@ const Manifesto = () => {
     <div className="pt-[76px] sm:pt-[96px]">
       <Head>
         <title>{t("manifesto_title")}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={t("manifesto_description")} />
-        <meta name="theme-color" content="default" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0 "
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&family=Rubik:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="description" content="home_description" />
+        <meta name="theme-color" content="default"></meta>
       </Head>
       <Header />
       <BlockWithBackground
