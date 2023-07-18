250ea295db8eedd3628e0c9f0036cbe4ed33c03a
devui3 <devui3@qoobus.com>
2023-07-18 13:11:56 +0300

    fix2

--- pages/team.tsx
+++ pages/team.tsx
@@ -38,12 +38,6 @@ const team = () => {
     <div className="pt-[80px] sm:pt-[96px]">
       <Head>
         <title>{t("team_title")}</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&family=Rubik:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={t("team_description")} />
         <meta name="theme-color" content="default"></meta>
