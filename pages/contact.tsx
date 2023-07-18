c73a5312d3c34b14c1217edd7533b8232cf05a41
devui3 <devui3@qoobus.com>
2023-07-18 11:35:19 +0300

    fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -26,8 +26,14 @@ const contact = () => {
       <Head>
         <title>{t("contact_title")}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={t("contact_description")} />
-        <meta name="theme-color" content="default" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&family=Rubik:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="description" content="home_description" />
+        <meta name="theme-color" content="default"></meta>
       </Head>
       <Header />
       <BlockWithBackground
--- pages/manifesto.tsx
+++ pages/manifesto.tsx
@@ -4,8 +4,6 @@ import { useTranslation } from "next-i18next";
 import getServerSideTranslations from "../utils/getServerSideTranslations";
 import { GetStaticPropsContext } from "next";
 
-import WhyAqords from "../components/WhyAqords/WhyAqords";
-import Footer from "../components/Footer/Footer";
 import BlockWithBackground from "../components/BlockWithBackground/BlockWithBackground";
 import { ManifestList } from "../components/Constants/WhyAqordsItems";
 import Header from "../components/Header/Header";
@@ -35,9 +33,18 @@ const Manifesto = () => {
     <div className="pt-[76px] sm:pt-[96px]">
       <Head>
         <title>{t("manifesto_title")}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={t("manifesto_description")} />
-        <meta name="theme-color" content="default" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0 "
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&family=Rubik:wght@500;700&display=swap"
+          rel="stylesheet"
+        />
+        <meta name="description" content="home_description" />
+        <meta name="theme-color" content="default"></meta>
       </Head>
       <Header />
       <BlockWithBackground
@@ -56,8 +63,8 @@ const Manifesto = () => {
         title={t("manifesto_page_title1")}
         desc1={t("manifesto_page_desc1")}
         desc2={t("manifesto_page_desc2")}
-        img={manifesto1}
-        imgStyle="object-cover absolute md:relative bottom-[-10%] right-[0%] sm:top-[0%] sm:right-[0%]"
+        img={manifesto}
+        imgStyle="absolute top-[65%] right-[0%] sm:top-[65%] sm:right-[0%] md:top-[0%] md:right-[-25%] lg:top-[0%] lg:right-[-0%]"
       />
       <div className="bg-second">
         <div className="container">
