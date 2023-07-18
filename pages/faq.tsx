209d323c713d69c74b2712b2a7ad31f9879b2164
devui3 <devui3@qoobus.com>
2023-07-18 13:11:56 +0300

    fix2

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -17,12 +17,6 @@ const Faq = () => {
     <>
       <Head>
         <title>{t("faq_title")}</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&family=Rubik:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={t("faq_description")} />
         <meta name="theme-color" content="default"></meta>
--- pages/whitepaperinfo.tsx
+++ pages/whitepaperinfo.tsx
@@ -14,13 +14,7 @@ const whitepapperinfo = () => {
   return (
     <>
       <Head>
-        <title>{t("whitepaperinfo_title")}</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&family=Rubik:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
+        <title>{t("whitepaper_title")}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="noindex"></meta>
         <meta name="theme-color" content="default"></meta>
