c66b9f8f7cf9dd41106efa2ec9bbde1f8a384f06
DragniArte <dev1@aqords.com>
2023-07-20 11:27:26 +0300

    changed pages, add meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,20 @@ import { useTranslation } from "next-i18next";
 import getServerSideTranslations from "../utils/getServerSideTranslations";
 import { GetStaticPropsContext } from "next";
 
-import Homepage from "./homepage";
+import Upperblock from "../components/Upperblock/Upperblock";
+import Navigationblock from "../components/Navigationblock/Navigationblock";
+import WhyAqords from "../components/WhyAqords/WhyAqords";
+import { whyAqordsList } from "../components/Constants/WhyAqordsItems";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
-export default function Home({ image }: any) {
+export default function Home() {
   const { t } = useTranslation();
   return (
     <>
       <Head>
         <title>{t("home_title")}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
@@ -18,10 +24,13 @@ export default function Home({ image }: any) {
           rel="stylesheet"
         />
         <meta name="description" content={t("home_description")} />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="default"></meta>
       </Head>
-      <Homepage />
+      <Header />
+      <Upperblock />
+      <WhyAqords itemsArray={whyAqordsList} />
+      <Navigationblock />
+      <Footer />
     </>
   );
 }
