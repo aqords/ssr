0b5be9cf78160d50579d8b9f59bae0840598580e
devui3 <devui3@qoobus.com>
2023-07-20 16:10:58 +0300

    fix fonts

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -18,7 +18,7 @@ const Faq = () => {
         <title>{t("faq_title")}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="home_description" />
-        <meta name="theme-color" content="default"></meta>
+        <meta name="theme-color" content="default"/>
       </Head>
       <Header />
       <section>
