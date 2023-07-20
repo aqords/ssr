2782cdb41c24802384aed66b54859c14273b0cc1
devui3 <devui3@qoobus.com>
2023-07-20 16:10:58 +0300

    fix fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home({ image }: any) {
         <title>{t("home_title")}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="home_description" />
-        <meta name="theme-color" content="default" />
+        <meta name="theme-color" content="default"/>
       </Head>
       <Homepage />
     </>
