6d96084be50a58362c4b1921f86dc9cc05fc49dc
devui3 <devui3@qoobus.com>
2023-07-20 14:55:52 +0300

    deleted fonts

--- pages/invest.tsx
+++ pages/invest.tsx
@@ -41,8 +41,8 @@ const invest = () => {
       <Head>
         <title>{t("invest_title")}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="robots" content="noindex"></meta>
-        <meta name="theme-color" content="default"></meta>
+        <meta name="description" content="home_description" />
+        <meta name="theme-color" content="default" />
       </Head>
       <Header />
       <BlockWithBackground
