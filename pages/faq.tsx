14ad2a499f96895044370206fc4d9141430cc4f8
devui3 <devui3@qoobus.com>
2023-07-20 14:55:52 +0300

    deleted fonts

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -19,12 +19,6 @@ const Faq = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="home_description" />
         <meta name="theme-color" content="default"></meta>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400&display=swap"
-        />
       </Head>
       <Header />
       <section>
