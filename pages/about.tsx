45d02d882db97fc50736b5aaa8f4611bfabe0da3
stratulaq <136684945+stratulaq@users.noreply.github.com>
2023-07-24 13:38:07 +0300

    Merge branch 'optimization-images' into MAIN

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,13 +3,16 @@ import Head from "next/head";
 import { useTranslation } from "next-i18next";
 import getServerSideTranslations from "../utils/getServerSideTranslations";
 import { GetStaticPropsContext } from "next";
+import dynamic from "next/dynamic";
 
-import InfoAboutblock from "../components/AboutUsblock/InfoAboutblock";
 import BlockWithBackground from "../components/BlockWithBackground/BlockWithBackground";
-import aboutusbg from "../public/assets/images/aboutbg.svg";
-import Footer from "../components/Footer/Footer";
+import aboutusbg from "../public/assets/images/aboutusbg.png";
 import Header from "../components/Header/Header";
 
+const InfoAboutblock = dynamic(
+  () => import("../components/AboutUsblock/InfoAboutblock")
+);
+const Footer = dynamic(() => import("../components/Footer/Footer"));
 const about = () => {
   const { t } = useTranslation();
 
@@ -36,7 +39,7 @@ const about = () => {
         desc1={t("about_page_desc1")}
         desc2={t("about_page_desc2")}
         img={aboutusbg}
-        imgStyle="absolute top-[250px] smx:top-[150px] smx:right-[-100px] smx:scale-[0.7] scale-[0.8] right-[-40px] sm:right-[-155px] sm:scale-[0.6] sm:top-[100px] md:top-[-20px]  md:right-[-250px] md:scale-[0.6] lg:scale-[1] lg:right-[-370px]  xl:right-[-230px] 2xl:right-[-170px] 3xl:right-[0px]"
+        imgStyle="object-cover absolute top-[250px] smx:top-[150px] smx:right-[-100px] smx:scale-[0.7] scale-[0.8] right-[-40px] sm:right-[-155px] sm:scale-[0.6] sm:top-[100px] md:top-[-20px]  md:right-[-250px] md:scale-[0.6] lg:scale-[1] lg:right-[-370px]  xl:right-[-230px] 2xl:right-[-170px] 3xl:right-[0px]"
       />
       <InfoAboutblock />
 
