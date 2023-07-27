2977612eb7d45f57f7bb670c1c36a5a54301a52d
devui3 <devui3@qoobus.com>
2023-07-27 11:25:07 +0300

    delete less fonts

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -17,12 +17,6 @@ const roadmap = () => {
     <div className="pt-[20px] sm:pt-[96px]">
       <Head>
         <title>{t("roadmap_title")}</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400&display=swap"
-          rel="stylesheet"
-        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={t("roadmap_description")} />
         <meta name="theme-color" content="default"></meta>
